c627a29a1fd2edd8189f40475d8192b16d4aedbb
Mouhssine Kinani <kinanimouhssine@gmail.com>
2026-07-20 02:04:43 +0100

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,20 @@
 import express from "express"
+import path from "path"
+
 import { ENV } from "./lib/env.js";
 
 const app = express();
+
+const __dirname = path.resolve();
+
+//make my app ready for deployment :
+if (ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    
+        app.get("/{*any}",(req,res)=>{
+            res.sendFile(path.join(__dirname,"../frontend/","dist","index.html"));
+        })
+}
 
 app.get("/",  (req,res)=>{
     res.status(200).json({msg:"sucess from backend"});
