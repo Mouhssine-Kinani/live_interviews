7f088a3f392e55ef3b2ec34d45a66c921a5fea54
Mouhssine Kinani <kinanimouhssine@gmail.com>
2026-07-20 03:00:40 +0100

    fixing the port in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,15 +11,15 @@ const __dirname = path.resolve();
 if (ENV.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")));
     
-        app.get("/{*any}",(req,res)=>{
-            res.sendFile(path.join(__dirname,"../frontend/","dist","index.html"));
-        })
+    app.get("/{*any}",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/","dist","index.html"));
+    })
 }
 
 app.get("/",  (req,res)=>{
     res.status(200).json({msg:"sucess from backend"});
 })
 
-app.listen(3000,()=>{
+app.listen(ENV.PORT,()=>{
     console.log(`server is runnin on http://localhost:${ENV.PORT}`);
 })
