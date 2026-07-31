1eb695ef48ba9817bb08370b7ce168c56667c9c5
Mouhssine Kinani <kinanimouhssine@gmail.com>
2026-07-31 02:34:13 +0100

    fix bug 3

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL || "/api",
     withCredentials: true, // browser will send cookies to the server in every request ( on every single request ), and the server can send cookies back to the browser
 });
 
